2e1fe2bbb7a3bde13cedee5fe6c87e53ba1fd311
Jono Sommers <jonosommers95@gmail.com>
2025-02-13 11:48:28 -0600

    buttons are working, upvote is working, downvote is not

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,38 @@
 import { useState } from 'react';
 import './App.css';
-import searchIcon from '../icons/search.png';
 import moviePosters from '../data/movie_posters';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 // Example imports (for later):
 // import { useState, useEffect } from 'react';
 // import movieDetails from '../data/movie_details';
+// import searchIcon from '../icons/search.png';
 
 function App() {
   const [movies, setMovies] = useState(moviePosters);
+
+  function upVote(id) {
+    setMovies(movies => movies.map((movie) =>
+      movie.id === id ? {...movie, vote_count: movie.vote_count += 1}
+      :movie
+      )
+    )
+  }
+
+  function downVote(id) {
+    setMovies(movies => movies.map((movie) => 
+      movie.id === id ? {...movies, vote_count: movie.vote_count -= 1 }
+      :movie
+      )
+    )
+  }
   
   return (
     <main className='App'>
       <header>
         <h1>Rancid Tomatillos</h1>
       </header>
-      <MoviesContainer movies = { movies } />
+      <MoviesContainer movies = { movies } upVote={ upVote } downVote={ downVote } />
     </main>
   );
 }
