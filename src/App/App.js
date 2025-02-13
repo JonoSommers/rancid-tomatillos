028758a4b060fd717610eaff36f9f1551fd319f2
Patrick Shim <patrick.shim91@yahoo.com>
2025-02-13 16:01:27 -0500

    feat: added home icon in MovieDetails to return back to home when clicking on the icon

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import searchIcon from '../icons/search.png';
+import homeIcon from '../icons/home.png';
 import moviePosters from '../data/movie_posters';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
@@ -10,6 +11,7 @@ import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
   const [movies, setMovies] = useState(moviePosters);
+<<<<<<< HEAD
   
   return (
     <main className='App'>
@@ -19,6 +21,34 @@ function App() {
       <MoviesContainer movies = { movies } />
     </main>
   );
+=======
+  const [clickedMovie, setClickedMovie] = useState(false);
+
+  const handleClick = () => {
+    setClickedMovie(movieDetails)
+  }
+
+  if (clickedMovie) {
+    return (
+      <main className='App'>
+        <header>
+          <h1>Rancid Tomatillos</h1>
+          <img className="homeIcon" src={homeIcon} alt="Back to home" onClick={ () => setClickedMovie(false) }></img>
+        </header>
+        <MovieDetails movie={clickedMovie} />
+      </main>
+    );
+  } else {
+    return (
+      <main className='App'>
+        <header>
+          <h1>Rancid Tomatillos</h1>
+        </header>
+        <MoviesContainer movies = { movies } onClick={handleClick}/>
+      </main>
+    );
+  }
+>>>>>>> d20cf19 (feat: added home icon in MovieDetails to return back to home when clicking on the icon)
 }
 
 export default App;
