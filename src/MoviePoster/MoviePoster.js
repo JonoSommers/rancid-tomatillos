491c42bca201a490e7f10ef6b93134e3b14af56d
Jono Sommers <jonosommers95@gmail.com>
2025-02-13 18:23:28 -0600

    Update MoviePoster.js
    changed on onClick prop value to handleClick to follow better naming conventions

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,9 +1,9 @@
 import './MoviePoster.css';
 
-function MoviePoster({poster_path, vote_count, onClick}) {
+function MoviePoster({poster_path, vote_count, handleClick}) {
   return (
     <section className='MoviePoster'>
-      <img src= { poster_path } alt= "Movie Poster" onClick={onClick}/>
+      <img src= { poster_path } alt= "Movie Poster" onClick={handleClick}/>
       <p> <button onClick={() => upVote(id) }>⬆️</button> { vote_count } <button onClick={() => downVote(id) }>⬇️</button></p>
     </section>
   );
